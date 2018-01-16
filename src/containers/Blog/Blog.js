dcc6e870a3f90d5dc643832fe1ee806831e8b9d0
Anderson Cossul <anderson_cossul@hotmail.com>
2018-01-15 22:23:58 -0200

    Mutating the received posts.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -14,13 +14,27 @@ class Blog extends Component {
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                this.setState({posts: response.data})
+                 // as this backend is not very flexible, I'm taking all the posts but not storing all
+                 // also adding a hard-coded author name after me for all of them
+                const posts = response.data.slice(0, 4)
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Anderson'
+                    }
+                })
+                this.setState({posts: updatedPosts})
             })
     }
 
     render () {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} />
+            return (
+                <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}/>
+            )
         })
 
         return (
