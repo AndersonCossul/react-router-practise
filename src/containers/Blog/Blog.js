6bf6c8a21a8e46891810ace390356a3799f542b8
Anderson Cossul <anderson@dexdigital.com.br>
2018-01-22 00:37:02 -0300

    Little restructure for routing.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,52 +1,9 @@
 import React, { Component } from 'react'
 import './Blog.css'
-import axios from 'axios'
+import Posts from './Posts/Posts'
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-    componentDidMount () {
-        axios.get('/posts')
-            .then(response => {
-                 // as this backend is not very flexible, I'm taking all the posts but not storing all
-                 // also adding a hard-coded author name after me for all of them
-                const posts = response.data.slice(0, 4)
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Anderson'
-                    }
-                })
-                this.setState({posts: updatedPosts})
-            })
-            .catch(error => {
-                this.setState({error: true})
-            })
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id})
-    }
-
     render () {
-        let posts = <p style={{textAlign: 'center', color: 'red', fontWeight: 'bold'}}>Something went wrong!</p>
-
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return (
-                    <Post
-                        key={post.id}
-                        title={post.title}
-                        author={post.author}
-                        clicked={() => this.postSelectedHandler(post.id)}/>
-                )
-            })
-        }
-
         return (
             <div className="Blog">
                 <header>
@@ -57,6 +14,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
+                <Posts/>
             </div>
         )
     }
---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import Post from '../../../components/Post/Post'
+import './Posts.css'
+import axios from 'axios'
+
+class Posts extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount () {
+        axios.get('/posts')
+            .then(response => {
+                 // as this backend is not very flexible, I'm taking all the posts but not storing all
+                 // also adding a hard-coded author name after me for all of them
+                const posts = response.data.slice(0, 4)
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Anderson C'
+                    }
+                })
+                this.setState({posts: updatedPosts})
+            })
+            .catch(error => {
+                console.log(error)
+                // this.setState({error: true})
+            })
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
+    render () {
+        let posts = <p style={{textAlign: 'center', color: 'red', fontWeight: 'bold'}}>Something went wrong!</p>
+
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return (
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        clicked={() => this.postSelectedHandler(post.id)}/>
+                )
+            })
+        }
+
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        )
+    }
+}
+
+export default Posts
