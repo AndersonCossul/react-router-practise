65dd61a28f161218cb9e7f02864b9c3dbfc1a0a6
Anderson Cossul <anderson@dexdigital.com.br>
2018-02-10 02:31:36 -0200

    Passing course title as search query param.

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 
 class Course extends Component {
   render () {
+    const queries = new URLSearchParams(this.props.location.search)
+    const title = queries.get('title')
+
     return (
       <div>
-        <h1>_COURSE_TITLE_</h1>
+        <h1>{title}</h1>
         <p>You selected the Course with ID: {this.props.match.params.id}</p>
       </div>
     );
