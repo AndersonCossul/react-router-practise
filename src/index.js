2bec13b55d9e8a19a82bf93118c5748a8754b1fb
Anderson Cossul <anderson@dexdigital.com.br>
2018-02-10 01:51:42 -0200

    Added template from course for the assignment.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import registerServiceWorker from './registerServiceWorker'
-import configAxios from './axios.config'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-configAxios()
-
-ReactDOM.render( <App />, document.getElementById( 'root' ) )
-registerServiceWorker()
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
